581381249a28a89512196352a269c52d4e67157d
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-07-17 13:24:31 +0200

    add task

---
+++ 0x00-hello-dart/2-print_number.dart
@@ -0,0 +1,5 @@
+void main() {
+  var n = 98;
+  var String = "Battery street\n";
+  print('$n $String');
+}
---
+++ 0x00-hello-dart/4-print_string.dart
@@ -0,0 +1,5 @@
+void main() {
+  String str = "Holberton School";
+  var output = '${str * 3}\n${str.substring(0, 9)}\n';
+  print(output);
+}
