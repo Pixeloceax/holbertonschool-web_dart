dcf0219ee509de31a07b49807377a0deaace8a4b
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-07-18 12:08:50 +0200

    pointers with S

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -2,11 +2,11 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   teamA.update('Free throws', (value) => value * 1, ifAbsent: () => 0);
   teamB.update('Free throws', (value) => value * 1, ifAbsent: () => 0);
 
-  teamA.update('2 pointer', (value) => value * 2, ifAbsent: () => 0);
-  teamB.update('2 pointer', (value) => value * 2, ifAbsent: () => 0);
+  teamA.update('2 pointers', (value) => value * 2, ifAbsent: () => 0);
+  teamB.update('2 pointers', (value) => value * 2, ifAbsent: () => 0);
 
-  teamA.update('3 pointer', (value) => value * 3, ifAbsent: () => 0);
-  teamB.update('3 pointer', (value) => value * 3, ifAbsent: () => 0);
+  teamA.update('3 pointers', (value) => value * 3, ifAbsent: () => 0);
+  teamB.update('3 pointers', (value) => value * 3, ifAbsent: () => 0);
 
   var teamAScore = teamA.values.reduce((a, b) => a + b);
   var teamBScore = teamB.values.reduce((a, b) => a + b);
