e2f7882917a8e732f5c2d3a63ee0d49430151d6e
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-07-20 15:26:00 +0200

    add task

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,18 @@
+class Password {
+  String password = "";
+
+  Password({required this.password});
+
+  bool isValid() {
+    if (password.length < 8) return false;
+    if (!password.contains(RegExp(r'[A-Z]'))) return false;
+    if (!password.contains(RegExp(r'[a-z]'))) return false;
+    if (!password.contains(RegExp(r'[0-9]'))) return false;
+    return true;
+  }
+
+  @override
+  String toString() {
+    return "Your Password is: $password";
+  }
+}
